38cf6219707b1c68fd70785772a888f67b771cc0
SSI STUDENT <SSI STUDENT@HP07-PC>
2019-03-13 16:15:15 +0530

    13-03-19-edited

--- src/main/java/com/ssi/Client.java
+++ src/main/java/com/ssi/Client.java
@@ -8,20 +8,7 @@ public class Client {
 	public static void main(String[] args) {
 		System.out.println("Starting Application");
 		ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
-		Customer c1=context.getBean("customer1",Customer.class);
-		c1.showInfo();
-		System.out.println("_______________________________________________________________");
-		Customer c2=context.getBean("customer2",Customer.class);
-		c2.showInfo();
-		System.out.println("_______________________________________________________________");
-
-		Product p1=context.getBean("product1",Product.class);
-		p1.showInfo();
-		
-		/*
-		System.out.println("_______________________________________________________________");
-		Supplier s1=context.getBean("supplier1",Supplier.class);
-		*/
+		ApplicationContext context1=new ClassPathXmlApplicationContext("spring.xml");
 	}
 }
 
---
+++ src/main/java/com/ssi/MyBeanFactoryPostProcessor.java
@@ -0,0 +1,17 @@
+package com.ssi;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+public class MyBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+
+	@Override
+	public void postProcessBeanFactory(ConfigurableListableBeanFactory arg0)
+			throws BeansException {
+
+		System.out.println("Inside postProcessBeanFactory");
+		
+	}
+
+}
